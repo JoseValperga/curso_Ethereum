cca791f0c5f840c24d78a8d9cda05ee45e0812f7
JoseValperga <jose.valperga@gmail.com>
2025-04-18 20:20:03 -0300

    Repaso de clases

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity 0.8.19;
 
 /**
- * @title Storage
+ * @title Storage - Jose Valperga
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
